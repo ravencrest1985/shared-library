05b94b967e7c6bf2ae9a40dc7ecc6cd71cee6528
ravencrest1985 <ravencrest1985@gmail.com>
2023-05-30 12:21:38 +0200

    Update backupApache.groovy

--- vars/backupApache.groovy
+++ vars/backupApache.groovy
@@ -1,3 +1,7 @@
 def call(Map config = [:]) {
-    bat "robocopy E:\\ptc\\Windchill_11.2\\HTTPServer F:\\apache_backup /MT /MIR"
+    bat """
+    robocopy E:\\ptc\\Windchill_11.2\\HTTPServer F:\\apache_backup /MT /MIR"
+    IF ERRORLEVEL 1 (echo ROBOCOPY STATUS = OK) ELSE (echo ROBOCOPY STATUS = FAILURE)
+    exit /B 0
+    """
 }
